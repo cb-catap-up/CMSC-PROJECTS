577af109c8c41c04257d7c5c9d28af5c4e079872
jom_mac <[john.a@portalink.com]>
2026-02-10 20:13:17 +0800

    adding new feature multiple file

--- CSMC-204/first_assignment/constants.py
+++ CSMC-204/first_assignment/constants.py
@@ -6,3 +6,7 @@ USER_PATH = os.path.join("database", "users.txt")
 START_SCREEN = "Place Holder"
 
 DESCRIPTION = 'description'
+
+CSV_PATH = os.path.join("Import", "patients_sample.csv")
+
+CSV_FOLDER_PATH = os.path.join("Import")
